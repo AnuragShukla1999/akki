3b3541c1eebc6e9d1741b49115cfdff116f83e70
Anurag Shukla <shuklaanurag310@gmail.com>
2024-08-19 16:29:41 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'http://192.168.29.93:3000',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true
